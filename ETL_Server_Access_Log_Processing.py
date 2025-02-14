012a04bb6b43e68ce76b8f6bfdbd9e6761554797
Nikesh Pathak <nikspathak91@gmail.com>
2025-02-14 01:02:55 -0500

    Extraction from downloaded file

--- ETL_Server_Access_Log_Processing.py
+++ ETL_Server_Access_Log_Processing.py
@@ -1,17 +1,27 @@
 import requests
 
 input_file = 'data/web-server-access-log.txt'
+extract_file = 'data/extracted-data.txt'
 
-url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"
-# Send a GET request to the URL
-with requests.get(url, stream=True) as response:
-    # Raise an exception for HTTP errors
-    response.raise_for_status()
-    # Open a local file in binary write mode
-    with open(input_file, 'wb') as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
-print(f"File downloaded successfully: {input_file}")
+def download_file():
+    url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"
+    # Send a GET request to the URL
+    with requests.get(url, stream=True) as response:
+        # Raise an exception for HTTP errors
+        response.raise_for_status()
+        # Open a local file in binary write mode
+        with open(input_file, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+    print(f"File downloaded successfully: {input_file}")
 
-
+def extract():
+    with open(input_file, 'r') as infile, \
+        open(extract_file, 'w') as outfile:
+        for line in infile:
+            fields = line.split('#')
+            if len(fields) >= 4:
+                field_1 = fields[0]
+                field_4 = fields[3]
+                outfile.write(field_1 + "#" + field_4 + "\n" )
 
