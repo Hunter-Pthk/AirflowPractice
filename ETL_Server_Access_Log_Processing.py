b00c221bb6a5c250911dab3d5212003388ae465e
Nikesh Pathak <nikspathak91@gmail.com>
2025-02-14 12:17:25 -0500

    All function set up for ETL

--- ETL_Server_Access_Log_Processing.py
+++ ETL_Server_Access_Log_Processing.py
@@ -3,6 +3,7 @@ import requests
 input_file = 'data/web-server-access-log.txt'
 extract_file = 'data/extracted-data.txt'
 transform_file = 'data/transformed.txt'
+load_file = 'data/capitalized.txt'
 
 def download_file():
     url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"
@@ -31,14 +32,31 @@ def extract():
 
 def transform():
     global extract_file, transform_file
+    # Read extracted file and write to transform file
     with open(extract_file, 'r') as infile, \
         open(transform_file, 'w') as outfile:
         for line in infile:
             processed_line = line.upper()
-            outfile.write(processed_line + "\n")
+            outfile.write(processed_line)
 
+def load():
+    global transform_file, load_file
+    print("Loading....")
+    with open(transform_file,'r') as infile, \
+        open(load_file, 'w') as outfile:
+        for line in infile:
+            outfile.write(line)
+
+def check():
+    global load_file
+    print("checking...")
+    with open(load_file, 'r') as infile:
+        for line in infile:
+            print(line)
 
 
 download_file()
 extract()
 transform()
+load()
+check()
